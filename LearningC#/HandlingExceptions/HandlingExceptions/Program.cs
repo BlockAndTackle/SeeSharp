22e7f49776e429fd0c946d41a0d67bebf91c1074
Brian Phillips <briandavidphillips@hotmail.com>
2017-06-23 10:41:30 -0400

    misc changes from Home PC

--- LearningC#/HandlingExceptions/HandlingExceptions/Program.cs
+++ LearningC#/HandlingExceptions/HandlingExceptions/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,39 @@ namespace HandlingExceptions
     {
         static void Main(string[] args)
         {
+            try
+            {
+                string strContent = File.ReadAllText(@"C:\home\brian\DEV\SeeSharp\LearningC#\Lessons from Bob Tabor (C# Fundamentals for Absolute Beginners)\Lesson_22\Exampl.txt");
+                Console.WriteLine(strContent); Console.WriteLine(""); Console.WriteLine("Press any key to end..."); Console.ReadKey();
+            }
+            catch (FileNotFoundException exError)
+            {
+                ErrorCatchAndRelease(exError);
+            }
+            catch (DirectoryNotFoundException exError)
+            {
+                ErrorCatchAndRelease(exError);
+            }
+            catch (Exception exError)
+            {
+                ErrorCatchAndRelease(exError);
+            }
 
         }
+
+        static void ErrorCatchAndRelease(Exception exFoo)
+        {
+
+            if (exFoo = FileNotFoundException)
+            {
+                string exMessage = "The file was not found - be sure to type the correct filename";
+            }
+            Console.WriteLine("There was a problem...");
+            Console.WriteLine("");
+            Console.WriteLine();
+            Console.ReadLine();
+            // throw;
+        }
+
     }
 }

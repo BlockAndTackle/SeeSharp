f2660cf6305912359f077bbbb4dbe6e84cc4f229
Brian Phillips <briandavidphillips@hotmail.com>
2017-06-27 16:46:46 -0400

    global repo changes added, committed, and pushed via giterdone.bat batch file

--- LearningC#/HandlingExceptions/HandlingExceptions/Program.cs
+++ LearningC#/HandlingExceptions/HandlingExceptions/Program.cs
@@ -9,47 +9,62 @@ namespace HandlingExceptions
 {
     class Program
     {
+        // Main method
         static void Main(string[] args)
         {
             try
             {
-                string strContent = File.ReadAllText(@"C:\home\brian\DEV\SeeSharp\LearningC#\Lessons from Bob Tabor (C# Fundamentals for Absolute Beginners)\Lesson_22\Exampl.txt");
-                Console.WriteLine(strContent); Console.WriteLine(""); Console.WriteLine("Press any key to end..."); Console.ReadKey();
+                string strContent = File.ReadAllText(@"C:\home\brian\DEV\SeeSharp\LearningC#\Lessons from Bob Tabor (C# Fundamentals for Absolute Beginners)\Lesson_22\Example.txt");
+                Console.WriteLine(strContent); Console.WriteLine("");
             }
-            catch (FileNotFoundException exError)
+            catch (FileNotFoundException ex)
             {
-                Console.WriteLine("There was a problem...");
-                Console.WriteLine("");
-                Console.WriteLine();
-                Console.ReadLine();
-                // ErrorCatchAndRelease(exError);
+                // Console.WriteLine("There was a problem...(FileNotFoundException)");
+                // Console.WriteLine(ex.Message);
+                // Console.ReadLine();
+                ErrorCatchAndRelease(ex);
             }
-            catch (DirectoryNotFoundException exError)
+            catch (DirectoryNotFoundException ex)
             {
-                
-                // ErrorCatchAndRelease(exError);
+                // Console.WriteLine("There was a problem...(DirectoryNotFoundException)");
+                // Console.WriteLine(ex.Message);
+                // Console.ReadLine();
+                ErrorCatchAndRelease(ex);
             }
-            catch (Exception exError)
+            catch (Exception ex)
             {
-                
-                // ErrorCatchAndRelease(exError);
+                // Console.WriteLine("There was a problem...(general exception)");
+                // Console.WriteLine(ex.Message);
+                // Console.ReadLine();
+                ErrorCatchAndRelease(ex);
+            }
+            // code to finalize the try-ctach - this code always runs - use it to set objects to null, close dtatabase connections, etc...
+            finally
+            {
+                Console.WriteLine(""); Console.WriteLine("Closing application now. Press any key to end..."); Console.ReadKey();
             }
 
         }
 
-        static void ErrorCatchAndRelease(Exception exFoo)
+        // method to evalutate the exception and report the error to the user before the system does
+        static void ErrorCatchAndRelease(Exception exException)
         {
-
-            if (exFoo = FileNotFoundException)
+            // string strExceptionMessage = exException.Message.ToString();
+            string strException = exException.GetType().ToString();
+            Console.WriteLine(exException.Message);
+            
+            if (strException == "System.IO.FileNotFoundException")
             {
-                string exMessage = "The file was not found - be sure to type the correct filename";
+                Console.WriteLine("Please make sure the file exists in the specified path...");
             }
-            Console.WriteLine("There was a problem...");
-            Console.WriteLine("");
-            Console.WriteLine();
-            Console.ReadLine();
-            // throw;
+            else if (strException == "System.IO.DirectoryNotFoundException")
+            {
+                Console.WriteLine("Please make sure the specified directory exists...");
+            }
+            else
+            {
+                Console.WriteLine("There was a general problem...");
+            }
         }
-
     }
 }
---
+++ LearningC#/TimerExample/TimerExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace TimerExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer myTimer = new Timer(2000);
+
+            myTimer.Elapsed += myTimer_Elapsed;
+
+            myTimer.Start();
+
+            Console.ReadLine();
+
+        }
+
+        static void myTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Elapsed: {0:HH:mm:ss.ffff}", e.SignalTime);
+            // throw new NotImplementedException();
+        }
+    }
+}

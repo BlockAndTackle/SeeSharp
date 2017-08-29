63aa6af91d6723282108f01bbbd4567d6aec9455
Brian Phillips <briandavidphillips@hotmail.com>
2017-08-29 14:14:45 -0400

    global repo changes added, committed, and pushed via giterdone.bat batch file

---
+++ CSharpCodeStorage/helloworlddotnetapp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace helloworlddotnetapp
+// new stupid test app created via new .Net instructions here: https://www.microsoft.com/net/core#windowscmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

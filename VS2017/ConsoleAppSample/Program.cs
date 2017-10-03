0115800d7b38213d923d3ca7ef32e979ea1380df
Brian Phillips <briandavidphillips@hotmail.com>
2017-10-03 18:59:22 -0400

    global repo changes added, committed, and pushed via giterdone.bat batch file

---
+++ VS2017/ConsoleAppSample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleAppSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			string strName;
+			Console.WriteLine("Please enter your name below:");
+			strName = Console.ReadLine();
+			Console.WriteLine($"Hello {strName}!!!");
+        }
+    }
+}

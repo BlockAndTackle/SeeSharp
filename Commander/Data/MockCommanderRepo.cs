fe4107f1c6b8dd52a5c51cb3eeb59a839e72e9d2
Brian David Phillips <bphillips@relias.com>
2021-10-06 10:25:06 -0400

    global repo changes added, committed, and pushed via giterdone.bat batch file

--- Commander/Data/MockCommanderRepo.cs
+++ Commander/Data/MockCommanderRepo.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using Commander.Models;
+
 namespace Commander.Data
 {
     // class that implements the ICommanderRepo interface
@@ -5,7 +8,20 @@ namespace Commander.Data
     // this is a not a derived class (which is inheritance)
     public class MockCommanderRepo : ICommanderRepo
     {
-        
+        public IEnumerable<Command> GetAppCommands()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Command GetCommandById(int Id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetCommandsByPlatform(string Platform)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 
 }
